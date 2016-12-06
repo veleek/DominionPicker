6ca2f2c642642c3a44ef58120b18cadc7854c01e
Ben Randall <veleek@gmail.com>
2016-12-05 23:44:18 -0800

    Moving Universal specific stuff to DominionCore.Universal

---
+++ DominionCore.Universal/Resources/en/CardDataStrings.Extensions.cs
@@ -0,0 +1,15 @@
+namespace Ben.Dominion.Resources
+{
+    using System.Globalization;
+
+    public partial class CardDataStrings
+    {
+        public static string GetString(string key)
+        {
+            //return resourceLoader.GetString(key);
+            throw new System.NotImplementedException();
+        }
+
+        public static CultureInfo Culture { get; set; }
+    }
+}
